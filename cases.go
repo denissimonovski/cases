e18606c0d6862ae8b262cdf4b590cad91116e105
Denis Simonovski <simonovskidenis3@gmail.com>
2018-12-24 20:28:33 +0100

    Case yearly report
    Signed-off-by: Denis Simonovski <simonovskidenis3@gmail.com>

--- cases.go
+++ cases.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/tealeg/xlsx"
 	"time"
 )
 
-var Day time.Duration = 24 * time.Hour
-var WorkDay time.Duration = 8 * time.Hour
+const Day time.Duration = 24 * time.Hour
+const WorkDay time.Duration = 8 * time.Hour
+const endOfWork int = 16
+const startOfWork int = 8
 
 func checkWorkTime(d time.Time) time.Time {
 	if d.Weekday() == time.Weekday(6) {
 		afterTwoDays := d.Add(Day * 2)
-		return time.Date(afterTwoDays.Year(), afterTwoDays.Month(), afterTwoDays.Day(), 8, 0, 0, 0, time.UTC)
+		return time.Date(afterTwoDays.Year(), afterTwoDays.Month(), afterTwoDays.Day(), startOfWork, 0, 0, 0, time.UTC)
 	} else if d.Weekday() == time.Weekday(0) {
-		afterOneDay := d.Add(time.Hour * 24)
-		return time.Date(afterOneDay.Year(), afterOneDay.Month(), afterOneDay.Day(), 8, 0, 0, 0, time.UTC)
-	} else if d.Hour() >= 16 {
 		afterOneDay := d.Add(Day)
-		return time.Date(afterOneDay.Year(), afterOneDay.Month(), afterOneDay.Day(), 8, 0, 0, 0, time.UTC)
-	} else if d.Hour() < 8 {
-		return time.Date(d.Year(), d.Month(), d.Day(), 8, 0, 0, 0, time.UTC)
+		return time.Date(afterOneDay.Year(), afterOneDay.Month(), afterOneDay.Day(), startOfWork, 0, 0, 0, time.UTC)
+	} else if d.Hour() >= endOfWork {
+		afterOneDay := d.Add(Day)
+		if afterOneDay.Weekday() == time.Weekday(6) {
+			afterThreeDays := d.Add(Day * 3)
+			return time.Date(afterThreeDays.Year(), afterThreeDays.Month(), afterThreeDays.Day(), startOfWork, 0, 0, 0, time.UTC)
+		}
+		return time.Date(afterOneDay.Year(), afterOneDay.Month(), afterOneDay.Day(), startOfWork, 0, 0, 0, time.UTC)
+	} else if d.Hour() < startOfWork {
+		return time.Date(d.Year(), d.Month(), d.Day(), startOfWork, 0, 0, 0, time.UTC)
 	}
 	return d
 }
@@ -28,9 +35,9 @@ func odzemi(s, e time.Time) time.Duration {
 	startTime := checkWorkTime(s)
 	endTime := checkWorkTime(e)
 	if startTime.Month() == endTime.Month() && startTime.Day() == endTime.Day() {
-		return e.Sub(s)
+		return endTime.Sub(startTime)
 	}
-	return razlicenDen(s, e) + (time.Duration(denovi(s, e)) * WorkDay)
+	return razlicenDen(startTime, endTime) + (time.Duration(denovi(startTime, endTime)) * WorkDay)
 }
 
 func denovi(s, e time.Time) int {
@@ -58,22 +65,35 @@ func razlicenDen(s, e time.Time) time.Duration {
 	return vreme1 + vreme2
 
 }
-func main() {
-	startDate := time.Date(2018, 11, 27, 13, 15, 0, 0, time.UTC)
-	endDate := time.Date(2018, 11, 28, 9, 15, 0, 0, time.UTC)
-	vreme := odzemi(startDate, endDate)
-	fmt.Println(vreme)
-	startDate = time.Date(2018, 11, 27, 13, 15, 0, 0, time.UTC)
-	endDate = time.Date(2018, 11, 29, 9, 15, 0, 0, time.UTC)
-	vreme = odzemi(startDate, endDate)
-	fmt.Println(vreme)
-	startDate = time.Date(2018, 11, 27, 13, 15, 0, 0, time.UTC)
-	endDate = time.Date(2018, 11, 30, 10, 15, 0, 0, time.UTC)
-	vreme = odzemi(startDate, endDate)
-	fmt.Println(vreme)
-	startDate = time.Date(2018, 11, 27, 13, 15, 0, 0, time.UTC)
-	endDate = time.Date(2018, 12, 4, 12, 15, 0, 0, time.UTC)
-	vreme = odzemi(startDate, endDate)
-	fmt.Println(vreme)
 
+func main() {
+	var VkupnoVreme time.Duration
+	xlFileName := "/root/go/src/cases/dookt.xlsx"
+	xlFile, err := xlsx.OpenFile(xlFileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	AdventSheet := xlFile.Sheet["AdventNetReport"]
+	for _, row := range AdventSheet.Rows {
+		startTime := parseTime(row.Cells[2].String())
+		var endTime time.Time
+		if row.Cells[3].String() == "-" {
+			//endTime = time.Date(2018, 31, 12, 16, 0, 0, 0, time.UTC)
+			continue
+		} else {
+			endTime = parseTime(row.Cells[3].String())
+		}
+		vremeCase := odzemi(startTime, endTime)
+		VkupnoVreme += vremeCase
+		fmt.Println(row.Cells[1:4], vremeCase.String())
+	}
+	fmt.Println(VkupnoVreme/time.Duration(len(AdventSheet.Rows)), len(AdventSheet.Rows))
+}
+
+func parseTime(s string) time.Time {
+	vreme, err := time.Parse("02-01-2006 15:04", s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return vreme
 }
